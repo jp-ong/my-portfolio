b9a96d2fd44f72259a79ff96dff2bedb1e0d368f
John Paul Ong <jpong5202@gmail.com>
2021-03-13 01:04:13 +0800

    Added navbar icons

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -5,22 +5,30 @@ const Navbar = ({ active }) => {
       <ul>
         <li>
           <Link href="#">
-            <a className={active === "intro" ? "active" : ""}>Home</a>
+            <a className={active === "intro" ? "active" : ""}>
+              <span className="fas fa-address-card"></span>Info
+            </a>
           </Link>
         </li>
         <li>
           <Link href="#about">
-            <a className={active === "about" ? "active" : ""}>About</a>
+            <a className={active === "about" ? "active" : ""}>
+              <span className="fas fa-user"></span>About
+            </a>
           </Link>
         </li>
         <li>
           <Link href="#skills">
-            <a className={active === "skills" ? "active" : ""}>Skills</a>
+            <a className={active === "skills" ? "active" : ""}>
+              <span className="fas fa-tools"></span>Skills
+            </a>
           </Link>
         </li>
         <li>
           <Link href="#projects">
-            <a className={active === "projects" ? "active" : ""}>Projects</a>
+            <a className={active === "projects" ? "active" : ""}>
+              <span className="fas fa-project-diagram"></span>Projects
+            </a>
           </Link>
         </li>
       </ul>
